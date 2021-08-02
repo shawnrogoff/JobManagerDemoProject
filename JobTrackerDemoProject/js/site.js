72002d16b7de796775fa3237b942274fb97451f1
ShawnRogoff <shawnrogoff@gmail.com>
2021-08-01 22:34:19 -0500

    Initial commit

---
+++ JobTrackerDemoProject/js/site.js
@@ -0,0 +1,8 @@
+// Hide Landing Page && Show Login Page
+function OpenLoginPage(){
+    document.getElementById("landingPage").classList.add("invisible");
+}
+// Show About Modal
+function OpenAboutPage(){
+    
+}
