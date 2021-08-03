b9b6883814ea8e923c394654b72fbe880a3c9b40
ShawnRogoff <shawnrogoff@gmail.com>
2021-08-02 23:18:03 -0500

    Added js control for moving to login p[age, jobs, and customer tables.

--- JobTrackerDemoProject/js/site.js
+++ JobTrackerDemoProject/js/site.js
@@ -1,8 +1,21 @@
 // Hide Landing Page && Show Login Page
 function OpenLoginPage(){
     document.getElementById("landingPage").classList.add("invisible");
+    document.getElementById("loginPage").classList.remove("invisible");
 }
-// Show About Modal
-function OpenAboutPage(){
+
+// Hide Login Page && Show Jobs Page
+function OpenJobsPage(){
+    document.getElementById("landingPage").classList.add("invisible");
+    document.getElementById("loginPage").classList.add("invisible");
+    document.getElementById("customersPage").classList.add("invisible");
+    document.getElementById("jobsPage").classList.remove("invisible");
     
+}
+
+function OpenCustomersPage(){
+    document.getElementById("landingPage").classList.add("invisible");
+    document.getElementById("loginPage").classList.add("invisible");
+    document.getElementById("jobsPage").classList.add("invisible");
+    document.getElementById("customersPage").classList.remove("invisible");
 }
